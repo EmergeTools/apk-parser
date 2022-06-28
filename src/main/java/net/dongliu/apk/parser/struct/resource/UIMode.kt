c4bf0c4f2cb3ccf034a894c0b84438f9fdebdba5
Maxi Rosson <maxirosson@gmail.com>
2022-06-27 22:34:31 -0300

    Add minApiLevel support to UIMode

--- src/main/java/net/dongliu/apk/parser/struct/resource/UIMode.kt
+++ src/main/java/net/dongliu/apk/parser/struct/resource/UIMode.kt
@@ -1,12 +1,12 @@
 package net.dongliu.apk.parser.struct.resource
 
-enum class UIMode(val flag: Int, val qualifier: String) {
-    DESK(2, "-desk"),
-    CAR(3, "-car"),
-    TELEVISION(4, "-television"),
-    APPLIANCE(5, "-appliance"),
-    WATCH(6, "-watch"),
-    VR_HEADSET(7, "-vrheadset");
+enum class UIMode(val flag: Int, val qualifier: String, val minApiLevel: Int) {
+    DESK(2, "-desk", 8),
+    CAR(3, "-car", 8),
+    TELEVISION(4, "-television", 13),
+    APPLIANCE(5, "-appliance", 16),
+    WATCH(6, "-watch", 20),
+    VR_HEADSET(7, "-vrheadset", 26);
 
     companion object {
         fun findQualifier(flag: Int): String {
